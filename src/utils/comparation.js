d0415fc980b25ca4d8af4b3b05949d42e42cea22
Jonas Freire <jonasfreireperson@gmail.com>
2020-05-08 22:22:24 -0300

    enhancement: sorting using B and C character

--- src/utils/comparation.js
+++ src/utils/comparation.js
@@ -1,9 +1,9 @@
 export default function psalmComparation(psalmOne, psalmTwo) {
-  let titleOne = psalmOne.title.match(/\d{1,3}[b|B]?/)[0];
-  let titleTwo = psalmTwo.title.match(/\d{1,3}[b|B]?/)[0];
+  let titleOne = psalmOne.title.match(/\d{1,3}[bBcC]?/)[0];
+  let titleTwo = psalmTwo.title.match(/\d{1,3}[bBcC]?/)[0];
 
-  titleOne = titleOne.replace(/[b|B]$/, '0000');
-  titleTwo = titleTwo.replace(/[b|B]$/, '0000');
+  titleOne = titleOne.replace(/[bBcC]$/, '0000');
+  titleTwo = titleTwo.replace(/[bBcC]$/, '0000');
 
   return Number(titleOne) - Number(titleTwo);
 }
